dce889f4e0b72ea5df6b62970be076a83970a6ac
chisomudeogu <chisomudeogu@yahoo.com>
2022-10-16 12:39:47 -0700

    printf

--- print_integer.c
+++ print_integer.c
@@ -1,76 +1,19 @@
 #include "main.h"
-/**
- * _abs - calculates the absolute value.
- * @number: input.
- * Return: value.
- */
-int _abs(int number)
-{
-	if (number < 0)
-		return (-1 * number);
-	else
-		return (number);
-}
 
 /**
- * contadordigit - counts the digits.
- * @number: input integer
- * Return: digit count
- */
-int contadordigit(int number)
+ * print_integer - Print a number in base 10
+ * @list: Number to print in base 10
+ *
+ * Return: Length of th numbers in decimal
+ **/
+int print_integer(va_list list)
 {
-	int count = 0;
-	int number2 = number;
+	char *p_buff;
+	int size;
 
-	if (number <= 0)
-		count += 1;
+	p_buff = itoa(va_arg(list, int), 10);
 
-	while (_abs(number2) != 0)
-	{
-		number2 = number2 / 10;
-		count++;
-	}
-	return (count);
-}
-/**
- * integer - a function that prints an integer.
- * @number: input integer
- * Return: digit count
- */
-int integer(int number)
-{
-	unsigned int unint;
-	int count;
+	size = print((p_buff != NULL) ? p_buff : "NULL");
 
-	count = contadordigit(number);
-if (number < 0)
-{
-_putchar('-');
-unint = -number;
-}
-	else
-		unint = number;
-
-	if (unint >= 10)
-		integer(unint / 10);
-	_putchar(unint % 10 + '0');
-
-	return (count);
-}
-
-/**
- * printint - prints a number
- * @format: format to print number
- * @pa: va_list with number to print
- * Return: number of characters printed
- */
-int printint(char *format, va_list pa)
-{
-	int number = va_arg(pa, int);
-	int numero;
-	(void)format;
-
-	numero = integer(number);
-
-	return (numero);
+	return (size);
 }
